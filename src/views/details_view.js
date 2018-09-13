f7bd08bd5ae16f541e962fcf379d9757523515eb
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-13 13:45:29 +0100

    adds display view rendering

--- src/views/details_view.js
+++ src/views/details_view.js
@@ -4,7 +4,21 @@ const DetailView = function (container, object) {
 }
 
 DetailView.prototype.display = function () {
-  console.log(this.object);
+    const name = document.createElement('h3');
+    name.textContent = `${this.object.name}`;
+    this.container.appendChild(name);
+
+    const list = document.createElement('ul');
+    this.container.appendChild(list);
+
+    const height = document.createElement('li')
+    height.textContent = `${this.object.height}`;
+    list.appendChild(height);
+
+    const meaning = document.createElement('li')
+    meaning.textContent = `${this.object.meaning}`;
+    list.appendChild(meaning);
+
 };
 
 module.exports = DetailView;
